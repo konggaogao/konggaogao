fdd9de160f69ae5a887fd9041853d240f763733f
konggaogao <951859925@qq.com>
2024-09-26 15:33:03 +0800

    Create mian.py

---
+++ mian.py
@@ -0,0 +1,9 @@
+import os
+
+os.system('rm -rf poc clone-templates')
+os.system('python 1-clone_repos.py')
+os.system('python 2-download_nuclei.py')
+os.system('python 3-check_poc.py')
+os.system('python 4-remove_duplicated.py')
+os.system('python 5-get_pocname.py')
+
